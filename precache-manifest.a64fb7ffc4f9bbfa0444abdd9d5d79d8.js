f141474f62b4fdc1bc5f89f0d349ab9ae423bf03
Full Stack Developer <riyan04314@gmail.com>
2021-01-26 12:40:59 +0530

    first commit

---
+++ precache-manifest.a64fb7ffc4f9bbfa0444abdd9d5d79d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d688dd3e8716754c8ddfd10f8cd03c1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58dfdb2d407d5e1a4ab2",
+    "url": "/static/css/main.d527a6ce.chunk.css"
+  },
+  {
+    "revision": "a450f8ff92f7ad07b15c",
+    "url": "/static/js/2.ffbe3001.chunk.js"
+  },
+  {
+    "revision": "58dfdb2d407d5e1a4ab2",
+    "url": "/static/js/main.827677c2.chunk.js"
+  },
+  {
+    "revision": "8d92adac7eabc56ce3dc",
+    "url": "/static/js/runtime~main.eaa07b19.js"
+  },
+  {
+    "revision": "6c4a83aa8a93532b28e0878060cc6b5e",
+    "url": "/static/media/logo.6c4a83aa.jpg"
+  }
+]);
